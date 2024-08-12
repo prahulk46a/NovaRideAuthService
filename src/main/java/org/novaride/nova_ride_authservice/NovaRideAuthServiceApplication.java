54b78acd077e7d87d7aaa14d86f0aff5ea843f0a
Akanksha Patil <[prahulk212@gmail.com]>
2024-08-12 22:53:47 +0530

    first commit

---
+++ src/main/java/org/novaride/nova_ride_authservice/NovaRideAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package org.novaride.nova_ride_authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaRideAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovaRideAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/novaride/nova_ride_authservice/NovaRideAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.novaride.nova_ride_authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaRideAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
